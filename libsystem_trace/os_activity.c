346c94b217d45a55d0d5323a2e2f178eca4fc450
Ariel Abreu <facekapow@outlook.com>
2020-06-15 22:27:39 -0400

    Add a stub for _os_activity_current

--- libsystem_trace/os_activity.c
+++ libsystem_trace/os_activity.c
@@ -5,3 +5,9 @@ void _os_activity_initiate(void *dso, const char *description, os_activity_flag_
 	// this is pretty much just a stub for now that allows the block to execute
 	activity_block();
 };
+
+// just a stub
+struct os_activity_s {
+	char just_a_dummy_member;
+};
+const struct os_activity_s _os_activity_current = {0};
